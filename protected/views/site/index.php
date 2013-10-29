50da8066a9b2e749dae3ec4834513f7ab184f4bd
boeuypeann <bpeann@gmail.com>
2013-10-29 20:44:14 +0700

    qqq

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -2,40 +2,6 @@
 /* @var $this SiteController */
 
 $this->pageTitle='Vsale Phone Shop';
+
 ?>
-  <div class="hero">
-    <div class="container">
-      <div class="row">
-        <div class="span12">
-
-          <div class="hero-left">
-            <i class="icon-desktop"></i>
-          </div>
-
-          <div class="hero-right">
-            <div class="hero-title">Howdy Dude! <b class="lightblue">Welcome</b> to the most amazing <b class="lightblue">Metro</b> Theme ever made in the history of Internet.</div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dictum condimentum magna, vel cursus turpis fermentum vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dictum condimentum magna, vel cursus turpis fermentum vel.</p>
-          </div>
-
-          <div class="clearfix"></div>
-        </div>
-      </div>
-    </div>
-  </div>
-
-
-<!-- Hero Ends -->
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
-
-<p>Congratulations! You have successfully created your Yii application.</p>
-
-<p>You may change the content of this page by modifying the following two files:</p>
-<ul>
-	<li>View file: <code><?php echo __FILE__; ?></code></li>
-	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
-</ul>
-
-<p>For more details on how to further develop this application, please read
-the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
+ <h1>Vsalephone welcomes !</h1>
--- protected/views/site/pages/about.php
+++ protected/views/site/pages/about.php
@@ -6,7 +6,19 @@ $this->breadcrumbs=array(
 	'About',
 );
 ?>
-<h1>About</h1>
+<h3>Vsalephone shop</h3>
 
-<p>This is a "static" page. You may change the content of this page
-by updating the file <code><?php echo __FILE__; ?></code>.</p>
+<p><strong>Vsalephone</strong> –  Mobile Phone Shop, established in 2013, has quickly become the Cambodia's leading mobile phone retailer at the most visible location of Siem Reap. 
+
+Goal of <strong>Vsalephone</strong> has always been to provide reliable products with high quality and to help customers minimize their times and costs in getting mobile handsets that are recommended the best fit their needs. 
+
+</br><br>We endeavor to continue to provide unlimited access to our customers to the information of brand-new and used mobile handsets and accessories. 
+
+Our valued customers can always gain extensive access to collective information which is useful and up-to-date about mobile phones of all popular brands. </p>
+
+<br>
+<h3>Our Vision</h3>
+<p>We continue to provide the best possible service, making sure that your buying experience is a good one. Our growth has been founded by the ability to deliver these benefits through focused customer service and a commitment to being at the very forefront of innovative products and services. 
+
+In the near future we will provide online order facility by which you can choose the products you like and place your order online and your ordered products are delivered to your address. You can save your times and costs spent to visit physical store. </p>
+<br>
